749d609d322624338616792a6cfe611385264fe3
iHadDream <451436177@qq.com>
2017-03-10 22:18:01 -0600

    科研立项-基于建筑内部的消防导航APP

---
+++ FireMan/src/com/example/fireman/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fireman;
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.phonegap.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
